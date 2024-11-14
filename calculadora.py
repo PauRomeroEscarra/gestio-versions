767a1901cde3b69771cb91b1c13807aea8f3c605
Pau <paur330@gmail.com>
2024-11-14 20:30:12 +0100

    vreso 2

--- calculadora.py
+++ calculadora.py
@@ -3,3 +3,12 @@ def sumar(a, b):
 
 def restar(a, b):
     return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: No es pot dividir per zero."
--- test_calculadora.py
+++ test_calculadora.py
@@ -1,5 +1,5 @@
 
-from calculadora import sumar, restar
+from calculadora import sumar, restar, multiplicar, dividir
 
 def test_sumar():
        assert sumar(2, 3) == 5
@@ -9,3 +9,14 @@ def test_restar():
        assert restar(3, 2) == 1
        assert restar(0, 0) == 0
        assert restar(5, 10) == -5
+
+def test_multiplicar():
+    assert multiplicar(2, 3) == 6
+    assert multiplicar(-1, 5) == -5
+    assert multiplicar(0, 100) == 0
+
+def test_dividir():
+    assert dividir(6, 3) == 2
+    assert dividir(5, 2) == 2.5
+    assert dividir(0, 1) == 0
+    assert dividir(1, 0) == "Error: No es pot dividir per zero."
